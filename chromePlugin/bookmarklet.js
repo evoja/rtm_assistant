617f072954239f70b40861b84efbdca32edd29bf
chivorotkiv <shmakov@omich.net>
2013-09-10 23:12:01 +0700

    [19] Рефакторинг существующего кода: делаем его читабельнее.

--- chromePlugin/bookmarklet.js
+++ chromePlugin/bookmarklet.js
@@ -1,1 +1,28 @@
-(function(){h='m.rememberthemilk.com';p='/add';if(window.getSelection){t=window.getSelection();}else if(document.getSelection){t=document.getSelection();}else if(document.selection){t=document.selection.createRange().text;};cp='http://'+h+p+'?name='+((null==t||t==""||t==undefined)?encodeURIComponent(document.title):t)+'&url='+encodeURIComponent(location.href);w=window.open(cp,'addwindow','status=no,toolbar=no,width=250,height=560,resizable=yes');setTimeout(function(){w.focus();},500);})();
+(function()
+{
+
+	var host = 'm.rememberthemilk.com'
+	var action = '/add'
+	var selection
+	if(window.getSelection)
+	{
+		selection = window.getSelection()
+	}
+	else if(document.getSelection)
+	{
+		selection = document.getSelection()
+	}
+	else if(document.selection)
+	{
+		selection = document.selection.createRange().text
+	};
+
+	var rtmUrl='http://' + host + action + '?name=' +
+		((null == selection || selection == "" || selection == undefined)
+			?encodeURIComponent(document.title)
+			:selection) +
+		'&url=' + encodeURIComponent(location.href)
+
+	var rtmWindow = window.open(rtmUrl, 'addwindow',
+		'status=no,toolbar=no,width=250,height=560,resizable=yes')
+})();
